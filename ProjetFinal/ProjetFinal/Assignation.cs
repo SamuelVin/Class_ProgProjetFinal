b1336e16acc8ed2ed46a5b2db3d7ce449230977d
jpq122 <ndjondo.agance20@yahoo.com>
2023-12-01 05:56:58 -0800

    toute les classes sont faites

--- ProjetFinal/ProjetFinal/Assignation.cs
+++ ProjetFinal/ProjetFinal/Assignation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAppSDK.Runtime.Packages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,46 @@ namespace ProjetFinal
 {
     internal class Assignation
     {
+
+        String matricule;
+        String numeroProjet;
+          int  nbrHeure;
+          double salPay ;
+
+        public Assignation()
+        {
+            this.Matricule = "";
+            this.NumeroProjet = "";
+            this.NbrHeure = 0;
+            this.SalPay = 0.0;
+             
+        }
+
+        public Assignation( string _matricule, string _numeroProjet, int _nbrHeure, double _salPay)
+        {
+            this.Matricule = _matricule;
+            this.NumeroProjet = _numeroProjet;
+            this.NbrHeure = _nbrHeure;
+            this.SalPay = _salPay;
+           
+        }
+
+
+        public string Matricule { get => matricule; set => matricule = value; }
+        public string NumeroProjet { get => numeroProjet; set => numeroProjet = value; }
+        public int NbrHeure { get => nbrHeure; set => nbrHeure = value; }
+
+        public double SalPay { get => salPay; set => salPay= value; }
+
+
+
+
+        public override string ToString()
+        {
+            return matricule + " " + numeroProjet + " " + nbrHeure + " " + salPay;
+        }
+
+
+
     }
 }
