f48cbdb15cf111d9aaa0864c455afae38f3c027b
jpq122 <ndjondo.agance20@yahoo.com>
2023-12-06 06:11:42 -0800

    Les pages ajouts de projet, client et employe sont faite. mais il ya quelques erreures. Je regarde avec le prof demain

--- ProjetFinal/ProjetFinal/Page_Employe.xaml.cs
+++ ProjetFinal/ProjetFinal/Page_Employe.xaml.cs
@@ -27,5 +27,125 @@ namespace ProjetFinal
         {
             this.InitializeComponent();
         }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            int valide = 0;
+
+            if (tbxMatriculeEmployé.Text.Trim() == "")
+            {
+
+
+                ErreurMatricule.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+            if (tbxNomEmployé.Text.Trim() == "")
+            {
+
+
+                ErreurEmployé.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+
+            if (tbxPrenomEmployé.Text.Trim() == "")
+            {
+
+
+                ErreurPrenom.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+
+
+            if (tbxDateNaissance.Text.Trim() == "")
+            {
+
+
+                ErreurDateNaissance.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+
+            if (tbxEmail.Text.Trim() == "")
+            {
+
+
+                ErreurEmail.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+
+            if (tbxAdresse.Text.Trim() == "")
+            {
+
+
+                ErreurAdresse.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+
+            if (tbxDateEmbauche.Text.Trim() == "")
+            {
+
+
+                ErreurDateEmbauche.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+
+            if (tbxTauxHoraire.Text.Trim() == "")
+            {
+
+
+                ErreurTauxHoraire.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+
+
+            if (tbxPhoto.Text.Trim() == "")
+            {
+
+
+                ErreurPhoto.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+
+            if (tbxStatut.Text.Trim() == "")
+            {
+
+
+                ErreurStatut.Visibility = Visibility.Visible;
+                valide += 1;
+
+            }
+
+
+            if (valide == 0)
+            {
+                SingletonEmploye.getInstance().AjouterEmployer(tbxMatriculeEmployé.Text, tbxNomEmployé.Text, tbxPrenomEmployé.Text, tbxDateNaissance.Text, tbxEmail.Text, tbxAdresse.Text, tbxDateEmbauche.Text, tbxTauxHoraire.Text, tbxPhoto.Text, tbxStatut.Text);
+                formEmployé1.Visibility = Visibility.Collapsed;
+                validation2.Visibility = Visibility.Visible;
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
